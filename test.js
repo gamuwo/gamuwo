36cd0fe0d773600c3ffa70590b91d321154e7c68
gamuwo <gamuwo@gmail.com>
2019-08-30 23:26:09 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1066,7 +1066,7 @@ class UI {
         'file save')}
         <a class="btn option" style="position:relative;">
           <input id="cookieGardenHelperFileLoadButton" type="file" 
-          style="cursor:pointer;opacity:0;position:absolute;left:0px;top:0px;width:100%;height:100%;" />
+          style="opacity:0;position:absolute;left:0px;top:0px;width:100%;height:100%;" />
           Load from file
         </a>
       </p>
