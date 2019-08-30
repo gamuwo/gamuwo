0cb4d38a84a522a016f6373ebe0351ca82e3f5d6
gamuwo <gamuwo@gmail.com>
2019-08-30 23:09:12 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1064,6 +1064,10 @@ class UI {
       <p>
         ${this.button('fileSaveButton', 'Save to file',
         'file save')}
+        <a class="btn option">
+          <input id="cookieGardenHelperFileLoadButton" type="file" style="cursor:pointer;opacity:0;position:absolute;left:0px;top:0px;width:100%;height:100%;" />
+          Load from file
+        </a>
       </p>
       <p>
         ${this.button('quickLoad', 'Quick load',
@@ -1209,6 +1213,11 @@ class UI {
     doc.elId('cookieGardenHelperPlotIsSaved').onmouseover = (event) => {
       Main.handleMouseoverPlotIsSaved(this);
     }
+    
+    doc.elId('cookieGardenHelperFileLoadButton').onchange = (event) => {
+      Game.FileLoad(event);
+    }
+    
   }
 
   static getSeedIconY(seedId) {
