461d228a4edc9cf9944ecdd1db5c7972ba0e9550
gamuwo <gamuwo@gmail.com>
2019-09-24 03:28:17 +0900

    Update test.js

--- test.js
+++ test.js
@@ -2201,7 +2201,7 @@ class UI {
       document.getElementById("cookieGardenHelperFileLoadButton").value = "";
     }
     
-    window.keydown = (event) => {
+    document.keydown = (event) => {
       console.log("event.keyCode:" + event.keyCode);
       console.log("event.ctrlKey:" + event.ctrlKey);
     }
